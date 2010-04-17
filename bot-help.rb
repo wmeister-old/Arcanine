9f8533dd67602d69b43b9fcc748e86433a328767
William Meister <wmeister86@gmail.com>
2010-04-17 03:32:45 -0400

    Initial commit :P

---
+++ bot-help.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'syntax/convertors/abstract'
+
+class HelpGenerator < Syntax::Convertors::Abstract
+  def convert(text)
+    commands = {}
+    cur_cmd  = nil
+
+    @tokenizer.tokenize(text) do |tok|
+      if tok.group == :comment
+        if tok =~ /^=begin/
+          lines = tok.split(/\r?\n/)[1..-2]
+        else
+          lines = [tok.sub(/^#/, '')]
+        end
+
+        for line in lines
+          line.strip!
+
+          if line =~ /^command:\s+(.*)/
+            cur_cmd           = $1
+            commands[cur_cmd] = {}
+          elsif line =~ /^alias(?:es)?:\s+(.*)/
+            commands[cur_cmd][:aliases] = $1.split /\s+/
+          elsif line =~ /^description:\s+(.*)/
+            commands[cur_cmd][:description] = $1
+          end
+        end
+      end
+    end
+    return commands
+  end
+end
