56cbbabb5a7b1f7af663451eb396e1f1031e420b
William Meister <wmeister86@gmail.com>
2010-04-20 01:06:12 -0400

    updated TODO; retabbed arcanine.rb

--- arcanine.rb
+++ arcanine.rb
@@ -2,7 +2,7 @@ require 'on_irc'
 require 'arcanine/trigger'
 
 class Arcanine
-	attr_reader :monitor, :trigger_char, :help
+  attr_reader :monitor, :trigger_char, :help
 
   def initialize(template)
     @trigger_char = template[:trigger_char] || template['trigger_char'] || '@'
@@ -11,7 +11,7 @@ class Arcanine
     @servers = __servers = template[:servers] || template['servers']
     @nick = __nick = template[:nick] || template['nick']
     @triggers = []
-		@monitor = {}
+    @monitor = {}
 
     @bot = IRC.new do
       nick     __nick
@@ -20,69 +20,69 @@ class Arcanine
 
       for s in __servers
         server s[:name] do
-					address s[:host]
-					port    s[:port]
-				end
+          address s[:host]
+          port    s[:port]
+        end
       end
     end
-	  load_triggers
+    load_triggers
   end
 
-	def trigger_files
-		Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/arcanine/trigger/*.rb')
-	end
+  def trigger_files
+    Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/arcanine/trigger/*.rb')
+  end
 
-	def load_triggers
-		files    = trigger_files
-		contents = ''
+  def load_triggers
+    files    = trigger_files
+    contents = ''
 
-		for file in files
-			begin 
-	 			require file
-			rescue Exception => e
-				puts "Error in #{file}: #{e}"
-				exit
-			end
-			contents << File.read(file)
-		end
+    for file in files
+      begin 
+        require file
+      rescue Exception => e
+        puts "Error in #{file}: #{e}"
+        exit
+      end
+      contents << File.read(file)
+    end
 
     @help = Arcanine::HelpGenerator.for_syntax('ruby').convert(contents)
-	end
+  end
 
-	def test_triggers(irc, msg)
-		puts "msg == #{msg}"
-		for trigger in Arcanine::Trigger.all
-			trigger.send :run, irc and break if trigger.send :match, msg
+  def test_triggers(irc, msg)
+    puts "msg == #{msg}"
+    for trigger in Arcanine::Trigger.all
+      trigger.send :run, irc and break if trigger.send :match, msg
     end
-	end
+  end
 
   def connect_all
     @bot.on :all do
       puts "[#{server.name}] #{sender}: #{command} - #{params.inspect}"
-		end
+    end
 
-		arcanine = self
+    arcanine = self
 
-		@bot.on :privmsg do
-			if params[0][0,1] == '#' && arcanine.monitor[server.name].include?(params[0])
-			  if params[1][0,1] == arcanine.trigger_char
-    			arcanine.test_triggers(self, params[1][1..-1])
-  			end
-			elsif params[0][0,1] != '#'
-				# do something
-			end
+    @bot.on :privmsg do
+      if params[0][0,1] == '#' && arcanine.monitor[server.name].include?(params[0])
+        if params[1][0,1] == arcanine.trigger_char
+          arcanine.test_triggers(self, params[1][1..-1])
+        end
+      elsif params[0][0,1] != '#'
+        # do something
+      end
     end
 
     @bot.on :ping do
       pong params[0]
     end
 
-		for s in @servers
-			@bot[s[:name]].on '001' do
-				s[:channels].each { |c|	join c }
-			end
-			@monitor[s[:name]] = s[:channels] || []
-		end
+    for s in @servers
+      @bot[s[:name]].on '001' do
+        s[:channels].each { |c| join c }
+      end
+      @monitor[s[:name]] = s[:channels] || []
+    end
 
     @bot.connect
   end
