df3d5c6a7a5da2d52b9b7d7787201b9d6b09a672
William Meister <wmeister86@gmail.com>
2010-04-20 13:35:05 -0400

    retabbed trigger.rb

--- arcanine/trigger.rb
+++ arcanine/trigger.rb
@@ -1,38 +1,38 @@
 class Arcanine
-	class Trigger
-  	@@triggers = []
-		@@help = nil
+  class Trigger
+    @@triggers = []
+    @@help = nil
 
-	  def self.inherited(subclass)
-  	  @@triggers << subclass
-  	end
+    def self.inherited(subclass)
+      @@triggers << subclass
+    end
 
-  	def self.all
-   	  @@triggers
-  	end
+    def self.all
+      @@triggers
+    end
 
-		def self.help(help = nil)
-			unless help.nil?
-				@@help = help
-			end
-			@@help
-		end
+    def self.help(help = nil)
+      unless help.nil?
+        @@help = help
+      end
+      @@help
+    end
 
-  	def self.regex
-    	raise 'Override me!'
-  	end
+    def self.regex
+      raise 'Override me!'
+    end
 
-  	def self.match(msg)
-    	@@matches =  self.regex.match msg
-    	msg       =~ self.regex
-  	end
+    def self.match(msg)
+      @@matches =  self.regex.match msg
+      msg       =~ self.regex
+    end
 
-  	def self.run(arcanine, irc)
-    	action(arcanine, irc, *@@matches[1..-1])
-  	end
+    def self.run(arcanine, irc)
+      action(arcanine, irc, *@@matches[1..-1])
+    end
 
-  	def self.action
-    	raise 'Override me!'
-  	end
-	end
+    def self.action
+      raise 'Override me!'
+    end
+  end
 end
